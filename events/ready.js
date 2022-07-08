28bf39013bb17fc4eda91f2414a3dc6e5e6dd489
Zyrxus <74361674+Zyrxus11@users.noreply.github.com>
2022-07-08 13:19:52 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const config = require('../config.js');
 module.exports = async client => {
-  client.user.setPresence({ activity: { type: "WATCHING", name: `DBL`}, status: 'idle' })
+  client.user.setPresence({ activity: { type: "WATCHING", name: `https://github.com/Zyrxus11/Discord-Bot-List`}, status: 'idle' })
 };
 
 
